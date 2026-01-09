fb8b32b66411b8d8801b61a0de9c629822928505
Steven R. White <srwhite@uci.edu>
2026-01-09 11:00:47 -0800

    Reduce regression test electron counts

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,8 +28,8 @@ end
     blocksize = 2
     cutoff = 1e-8
 
-    Nup = 2
-    Ndn = 2
+    Nup = 1
+    Ndn = 1
     psiup0 = orthonormal_cols(rng, N, Nup)
     psidn0 = orthonormal_cols(rng, N, Ndn)
     _, _, e_new = solve_hfdmrg(H, V, psiup0, psidn0;
@@ -38,7 +38,7 @@ end
         blocksize, maxiter, cutoff; verbose = false)
     @test isapprox(e_new, e_legacy; atol = 1e-6, rtol = 0)
 
-    Nup_r = 2
+    Nup_r = 1
     psiup_r = orthonormal_cols(rng, N, Nup_r)
     _, _, e_new_r = solve_hfdmrg(H, V, psiup_r;
         maxiter = maxiter, blocksize = blocksize, cutoff = cutoff, verbose = false)
